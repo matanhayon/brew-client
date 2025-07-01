77d7ae74fac224d62389f5298b09e601bc068093
matanhayon <matanhayon@gmail.com>
2025-07-02 01:24:53 +0300

    Fixed vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-/// <reference types="node" />
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+// Fix for __dirname in ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
