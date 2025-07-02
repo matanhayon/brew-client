099b6461f330e64e9f14b74b4c22a32d253799b9
matanhayon <matanhayon@gmail.com>
2025-07-02 15:33:51 +0300

    fixed theme for clerk

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,41 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { dark } from "@clerk/themes";
+import { ThemeProvider, useTheme } from "./context/theme-provider.tsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+
+function ClerkWithThemeProvider() {
+  const { theme } = useTheme();
+
+  const resolvedTheme =
+    theme === "system"
+      ? window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light"
+      : theme;
+
+  return (
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      appearance={{
+        ...(resolvedTheme === "dark" && { baseTheme: dark }),
+      }}
+      afterSignOutUrl="/"
+    >
       <App />
     </ClerkProvider>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <ClerkWithThemeProvider />
+    </ThemeProvider>
   </StrictMode>
 );
