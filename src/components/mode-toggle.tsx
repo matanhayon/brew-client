dd236a174c815e9b0c0bbae52f3efd7c76be54af
matanhayon <matanhayon@gmail.com>
2025-07-02 15:04:20 +0300

    fixed system dark mode

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -28,9 +28,11 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
+        {/* 
         <DropdownMenuItem onClick={() => setTheme("system")}>
           System
         </DropdownMenuItem>
+        */}
       </DropdownMenuContent>
     </DropdownMenu>
   );
