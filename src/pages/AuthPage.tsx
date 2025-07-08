289b7c05b90e1840d8ddb0ab3dbe2c1760a9ae48
matanhayon <matanhayon@gmail.com>
2025-07-08 12:41:20 +0300

    fixed mobile for auth

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -3,7 +3,7 @@ import { SignIn, SignUp } from "@clerk/clerk-react";
 
 export default function AuthPage() {
   return (
-    <div className="max-w-md mx-auto mt-3 p-6 border rounded-xl bg-background shadow-md">
+    <div className="max-w-md mx-auto p-1 rounded-xl bg-background shadow-md">
       <Tabs defaultValue="sign-in" className="w-full">
         <TabsList className="grid w-full grid-cols-2 mb-4">
           <TabsTrigger value="sign-in">Sign In</TabsTrigger>
